7feb3dee4962a20239692e3bc13f9939c9992511
Shamsundar12316 <168196650+Shamsundar12316@users.noreply.github.com>
2024-08-06 02:25:00 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+// part 7/ question 11 friend list
+Object.defineProperty(exports, "__esModule", { value: true });
+let friends = ["ayesha", "mavesh", "kiran", "dev", "lal"];
+for (let i = 0; i < friends.length; i++) {
+    console.log(friends[i]);
+}
---
+++ main.ts
@@ -0,0 +1,6 @@
+// part 7/ question 11 friend list
+
+let friends: string[] = ["ayesha","mavesh","kiran","dev","lal"];
+for(let i=0; i<friends.length; i++){
+  console.log(friends[i]);
+}
